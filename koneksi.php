b7e0e97c42a355ac2a2958669cdc8a55463ea325
Fransiskus Andika Setiawan <fransiskus.andika@bukalapak.com>
2021-03-31 21:28:35 +0700

    Kumpul Tugas

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,3 @@
 <?php 
-// koneksi dengan mysqli
-
-// isi nama host, username mysql, password mysql, dan nama database anda
 $con = mysqli_connect("localhost", "root", "", "latihandb");
-
-
-/*
-Code by YukCoding Tutor
-www.yukcoding.id
-*/
 ?>
